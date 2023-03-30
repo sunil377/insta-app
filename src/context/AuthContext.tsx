8919ca42555ff52f350a8137ee620e15114a1ad9
sunil panwar <sunilpanwar1996@gmail.com>
2023-03-30 22:03:26 +0530

    remove loading state in AuthContext

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -20,32 +20,23 @@ function useAuth() {
 
 function AuthProvider({ children }: { children: ReactNode }) {
     const [user, setUser] = useState<contextType>(null)
-    const [isLoading, setLoading] = useState(true)
 
     useEffect(() => {
         return onAuthStateChanged(auth, async (user) => {
             let token = ''
-
-            if (user) {
-                try {
+            try {
+                if (user) {
                     token = await user.getIdToken()
-                } catch (error) {
-                    console.error(error)
                 }
+            } catch (error) {
+                console.error(error)
             }
-
             nookies.set(null, 'token', token, { path: '/' })
-
             setUser(user)
-            setLoading(false)
         })
     }, [])
 
-    return (
-        <AuthContext.Provider value={user}>
-            {isLoading ? 'Loading...' : children}
-        </AuthContext.Provider>
-    )
+    return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>
 }
 
 export default AuthProvider
