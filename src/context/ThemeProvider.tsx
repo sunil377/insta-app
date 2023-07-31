5d5d6e49cd148c8f058b1ab10c5e31bb1f6f2631
sunil panwar <sunilpanwar1996@gmail.com>
2023-07-31 11:52:54 +0530

    fresh start going to imporve code

--- src/context/ThemeProvider.tsx
+++
@@ -1,9 +0,0 @@
-import { ThemeProvider } from 'next-themes'
-import { ThemeProviderProps } from 'next-themes/dist/types'
-
-export default function ThemeProvier({
-    children,
-    ...props
-}: ThemeProviderProps) {
-    return <ThemeProvider {...props}>{children}</ThemeProvider>
-}
